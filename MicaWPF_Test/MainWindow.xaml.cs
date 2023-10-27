0d99b4def4642866e9288390bc8a3018ab487edc
jimnastic89 <87902634+jimnastic89@users.noreply.github.com>
2023-10-27 14:19:36 +1100

    Add files via upload

---
+++ MicaWPF_Test/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MicaWPF.Controls;
+
+namespace MicaWPF_Test
+{
+    public partial class MainWindow : MicaWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
